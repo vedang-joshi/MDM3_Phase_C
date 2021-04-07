b192e1f6cfafe8a869e83bf79199edf76764a236
jake-beardo <jake.beard@hotmail.com>
2021-04-07 10:09:41 +0100

    Update transforming_data.py

--- Jake_Wavelet/transforming_data.py
+++ Jake_Wavelet/transforming_data.py
@@ -15,5 +15,6 @@ cData1, tcData1 = readFile(cData1)
 split_by_secs = 60
 remainder = len(tbData1) % split_by_secs
 tbData1 = tbData1[:-remainder]
-split_data = [tbData1[x:x+split_by_secs] for x in range(0, len(tbData1), split_by_secs)]
+split_data = np.array([tbData1[x:x+split_by_secs] for x in range(0, len(tbData1), split_by_secs)])
+split_data.transpose()
 print(split_data)
