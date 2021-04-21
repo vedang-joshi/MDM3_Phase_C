684bf0cbf2227a4ffb699573124f503a47e9fbb9
WillTipping <73531593+WillTipping@users.noreply.github.com>
2021-04-21 12:29:23 +0100

    Predicting drowning

--- Simple_Model/Simple_model.py
+++ Simple_Model/Simple_model.py
@@ -31,12 +31,37 @@ file = "Breast_data1.txt"
 read = readFile(file)
 
 df = pd.DataFrame(read)
-print(df)
 
 df = df.drop('tfps', axis=1)
+df = df.drop(df.index[328:628])
+
+# print(df)
+
+x = np.array(df)
+
+y = np.zeros([len(df),1])
+y[127:186] = 1
+
+y_train = np.zeros([len(df),1])
+
+for i in range(len(df)):
+    if x[i,1] > 1:
+        y_train[i] = 1
+
+print(np.mean(y == y_train))
+
+
+# 0-127 breast
+# 127-186 drowning
+# 186-327 breast
+# 327-628 standing
 
 # df = pd.read_csv("Breast_data1.txt")
 # df = df[23:-1]
 # print(df[0])
 # df.plot()
 
+# df.plot();
+# plt.xlabel('time every 200ms');
+# plt.legend(loc='best')
+# plt.show()
