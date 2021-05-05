e5760a9f9d4f8a9dfc91cea65b6cefbc08e3a351
WillTipping <73531593+WillTipping@users.noreply.github.com>
2021-05-05 09:50:56 +0100

    Looks at breast_drowning1 and breast_swimming1 data

--- Simple_Model/Simple_model.py
+++ Simple_Model/Simple_model.py
@@ -30,33 +30,51 @@ def readFile(file):
 file = "Breast_data1.txt"
 file2 = "../Breast_data2.txt"
 file3 = "../Crawl_data1.txt"
+file4 = "../breast_drowning1.txt"
+file5 = "../breast_swimming1.txt"
 
 # read = readFile(file)
 # read = readFile(file2)
-read = readFile(file3)
+# read = readFile(file3)
+read = readFile(file4)
+read2 = readFile(file5)
 
 df = pd.DataFrame(read)
 df = df.drop('tfps', axis=1)
+df2 = pd.DataFrame(read2)
+df2 = df2.drop('tfps', axis=1)
 
 # df = df.drop(df.index[328:628]) # Breast data1
 # df = df.drop(df.index[421:449]) # Breast data2
-df = df.drop(df.index[359:445]) # Crawl data1
+# df = df.drop(df.index[359:445]) # Crawl data1
 
 x = np.array(df)
 
-y = np.zeros([len(df),1])
+x2 = np.array(df2)
 
+y = np.zeros([len(df),1])
+y2 = np.zeros([len(df2),1])
+
+# y set to one when drowning
 # y[127:186] = 1 # Breast data1
 # y[175:243] = 1 # Breast data2
-y[159:229] = 1 # Crawl data1
+# y[159:229] = 1 # Crawl data1
+y[:] = 1
 
 y_train = np.zeros([len(df),1])
+y2_train = np.zeros([len(df2),1])
 
 for i in range(len(df)):
     if x[i,1] > 1:
         y_train[i] = 1
+for i in range(len(df2)):
+    if x2[i,1] > 1:
+        y2_train[i] = 1
 
-print(np.mean(y == y_train))
+mean1 = np.mean(y == y_train)
+mean2 = np.mean(y2 == y2_train)
+# print(np.mean(y == y_train))
+print(np.mean([mean1, mean2]))
 
 # --- Breast_data1 -----
 # 0-127 breast
@@ -76,7 +94,7 @@ print(np.mean(y == y_train))
 # 229-358 breast
 # 358-444 standing
 
-# read = readFile(file3)
+# read = readFile(file4)
 #
 # df = pd.DataFrame(read)
 # df = df.drop('tfps', axis=1)
