6ba37d804d8f56a5f63eb2517c651d77d95ec8d1
WillTipping <73531593+WillTipping@users.noreply.github.com>
2021-05-05 13:46:10 +0100

    Uses heart rate data aswell

--- Simple_Model/Simple_model.py
+++ Simple_Model/Simple_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 import re
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 def readFile(file):
 
@@ -32,12 +33,25 @@ file2 = "../Breast_data2.txt"
 file3 = "../Crawl_data1.txt"
 file4 = "../breast_drowning1.txt"
 file5 = "../breast_swimming1.txt"
+file6 = "../breast__1.txt"
+file7 = "../crawl__3.txt"
+file8 = "../crawl_drowning1.txt"
+file9 = "../crawl_swimming1.txt"
+
+vedang_drown = "../Vedang_Accelerometer_Crawl/26-28Apr_accelerometer_heart_rate_data/crawl__25_drowning.txt"
+vedang_swim = "../Vedang_Accelerometer_Crawl/26-28Apr_accelerometer_heart_rate_data/crawl__25_swimming.txt"
+heart_file_loc = "../Vedang_Accelerometer_Crawl/26-28Apr_accelerometer_heart_rate_data/heart_rate_data.csv"
+
+
+drown_heart = [98, 99, 100]
+swim_heart = [70, 70, 70, 116, 114, 108, 101, 96, 94, 92, 91, 95]
+
 
 # read = readFile(file)
 # read = readFile(file2)
 # read = readFile(file3)
-read = readFile(file4)
-read2 = readFile(file5)
+read = readFile(vedang_drown)
+read2 = readFile(vedang_swim)
 
 df = pd.DataFrame(read)
 df = df.drop('tfps', axis=1)
@@ -47,6 +61,11 @@ df2 = df2.drop('tfps', axis=1)
 # df = df.drop(df.index[328:628]) # Breast data1
 # df = df.drop(df.index[421:449]) # Breast data2
 # df = df.drop(df.index[359:445]) # Crawl data1
+
+N = len(df)/len(drown_heart)
+N2 = len(df2)/len(swim_heart)
+print(N)
+print(N2)
 
 x = np.array(df)
 
@@ -64,12 +83,14 @@ y[:] = 1
 y_train = np.zeros([len(df),1])
 y2_train = np.zeros([len(df2),1])
 
+print(math.floor(730/N))
 for i in range(len(df)):
-    if x[i,1] > 1:
+    if x[i,1] > 1 and drown_heart[math.floor(i/N)] > 90:
         y_train[i] = 1
 for i in range(len(df2)):
-    if x2[i,1] > 1:
+    if x2[i,1] > 1 and swim_heart[math.floor(i/N2)] > 90:
         y2_train[i] = 1
+
 
 mean1 = np.mean(y == y_train)
 mean2 = np.mean(y2 == y2_train)
@@ -94,12 +115,20 @@ print(np.mean([mean1, mean2]))
 # 229-358 breast
 # 358-444 standing
 
-# read = readFile(file4)
+# ------------- Plotting Section ----------------------------
+# read = readFile(vedang_swim)
 #
 # df = pd.DataFrame(read)
 # df = df.drop('tfps', axis=1)
 #
-# df.plot();
-# plt.xlabel('time every 200ms');
+# # xx = [0, len(df)]
+# # yy = [1, 1]
+#
+# df.plot()
+# # plt.plot(xx,yy, linewidth=3, label="Threshold Line")
+# plt.xlabel('Time (in 200ms steps)', fontsize=12)
+# plt.ylabel('Acceleration (m/s^2)', fontsize=12)
+# # plt.title("Plot of Raw Data with Threshold Line", fontsize=13)
 # plt.legend(loc='best')
 # plt.show()
+# ------------- Plotting Section ----------------------------
